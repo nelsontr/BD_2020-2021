ce550c6cd92134945fd6d9122cda9793d33396b5
catasofia <47949684+catasofia@users.noreply.github.com>
2020-11-14 17:35:04 +0000

    Update queries.sql

--- Entrega3/queries.sql
+++ Entrega3/queries.sql
@@ -1,7 +1,19 @@
 --####  ADDING QUERRIES TO PROBLEMS  ####
 
+-- Qual o concelho onde se fez o maior volume de vendas hoje?
 select nome
 from concelho 
-    NATURAL JOIN instituicao (alter?)
+    INNER JOIN instituicao
+    ON instituicao.num_regiao = concelho.num_regiao
+    AND instituicao.num_concelho = concelho.num_concelho
+    INNER JOIN venda_farmacia
+    ON venda_farmacia.inst = instituicao.nome
+WHERE venda_farmacia.data_registo = CAST(CURRENT_TIMESTAMP AS DATE)
+GROUP BY nome
+HAVING count(*) >= all (
+    SELECT count(*)
+    FROM concelho
+    GROUP BY nome);
     
-    
+--Qual o médico que mais prescreveu no 1º semestre de 2019 em cada região?
+
