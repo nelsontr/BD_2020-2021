3aadba9ed2c7256a47458763cfbe14d4ea64e871
kylazz <rodrigormajor@gmail.com>
2020-12-04 01:26:51 +0000

    added f_presc_venda on etl.sql

--- Entrega4/etl.sql
+++ Entrega4/etl.sql
@@ -18,3 +18,21 @@ SELECT inst.nome,
 		inst.num_regiao,
 		inst.num_concelho
 FROM instituicao inst
+
+With temp as (
+	SELECT num_cedula, num_doente, id_tempo, substancia, num_venda
+	FROM prescricao_venda pv 
+	INNER JOIN d_tempo dt
+	ON (
+		dt.ano = extract(year from pv.data) and
+		dt.mes = extract(month from pv.data) and
+		dt.dia = extract(day from pv.data))
+)
+INSERT INTO f_presc_venda(id_presc_venda, id_medico, num_doente, id_data_registo, id_inst, substancia, quant)
+SELECT t.num_venda, num_cedula, num_doente, id_tempo, id_inst, substancia, quant
+FROM temp t
+INNER JOIN (SELECT num_venda, id_inst, quant
+	FROM venda_farmacia vf
+	INNER JOIN d_instituicao di
+	ON vf.inst = di.nome) t2
+ON t.num_venda = t2.num_venda
